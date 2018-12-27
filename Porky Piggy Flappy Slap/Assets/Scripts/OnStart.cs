6e0b30d2239a94580bdca4b674c06fb06cacf7fc
PumpShotty64 <jordanguyent@gmail.com>
2018-12-26 20:11:25 -0800

    pop

---
+++ Porky Piggy Flappy Slap/Assets/Scripts/OnStart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Start_text;
+    void Start()
+    {
+
+        gameObject.GetComponent<Wall_Movement>().enabled = false;
+        gameObject.GetComponent<Movement>().enabled = false;
+
+        gameObject.GetComponent<Score>().enabled = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Wall_Movement>().enabled = true;
+            gameObject.GetComponent<Movement>().enabled = true;
+            gameObject.GetComponent<Score>().enabled = true;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 2.5f;
+            gameObject.GetComponent<OnStart>().enabled = false;
+            Start_text.GetComponent<Text>().text = "";
+        }
+    }
+}
---
+++ Porky Piggy Flappy Slap/Assets/Scripts/Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    private float time;
+    private float time2;
+    public int score;
+    public GameObject ScoreUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 3)
+        {
+            time2 += Time.deltaTime;
+            CountScore();
+            DisplayScore();
+        }
+    }
+
+    void CountScore()
+    {
+        if (time2 > 1)
+        {
+            score++;
+            time2 = 0;
+        }
+    }
+
+    void DisplayScore()
+    {
+        ScoreUI.GetComponent<Text>().text = ("Score: " + score);
+    }
+}

a2851eb3e27eb66040aa5afe6c5590a02d1d0f11
PumpShotty64 <jordanguyent@gmail.com>
2018-12-26 18:43:11 -0800

    Created Obstacles (Beta)

---
+++ Porky Piggy Flappy Slap/Assets/Scripts/Wall_Delete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Delete : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Delete();
+    }
+
+    void Delete() {
+        if (gameObject.transform.position.x < -15) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Porky Piggy Flappy Slap/Assets/Scripts/Wall_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Movement : MonoBehaviour
+{
+
+    public GameObject Wall;
+    private float time;
+    public float wallX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateWall();
+    }
+
+    void CreateWall()
+    {
+        time += Time.deltaTime;
+        if (time > 1)
+        {
+            GameObject wallBottom = Instantiate(Wall);
+            GameObject wallTop = Instantiate(Wall);
+            wallBottom.transform.position = new Vector2(15, Random.Range(-7, -1));
+            wallBottom.GetComponent<Rigidbody2D>().velocity = new Vector2(wallX, 0);
+            wallTop.transform.position = new Vector2(15, wallBottom.transform.position.y + 10);
+            wallTop.GetComponent<Rigidbody2D>().velocity = new Vector2(wallX, 0);
+            time = 0;
+        }
+        
+    }
+}
